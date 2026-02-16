87e6c45948b1e946af40a49d1f91e28df6d8acd9
Kangchanghyeon <changhyeon9125@gmail.com>
2026-02-16 17:10:33 +0900

    모바일 레이아웃 수정 (planner flex-col)

--- src/app/planner/layout.tsx
+++ src/app/planner/layout.tsx
@@ -6,7 +6,7 @@ export default function PlannerLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen bg-[#F9FAFB]">
+    <div className="flex flex-col min-h-screen bg-[#F9FAFB] md:flex-row">
       <PlannerSidebar />
       <main className="flex-1 overflow-auto">{children}</main>
     </div>
